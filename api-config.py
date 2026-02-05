4ff3af01a1105c124e26db1bd78f6749380c0723
Ibraheem Olaniyi Lawal <lawalibraheem19@gmail.com>
2026-02-04 22:54:06 -0300

    Docstring for  APIConfig

--- api-config.py
+++ api-config.py
@@ -1,4 +1,7 @@
 class APIConfig:
+    """
+    Docstring for APIConfig
+    """
     def __init__(self, api_key, model="gpt-3.6-turbo", max_tokens=100):
         self.api_key = api_key
         self.model = model
